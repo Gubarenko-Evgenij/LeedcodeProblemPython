42df2e699e92ffd15ea20f5412fd7da806d6b00b
Gubarenko-Evgenij <gubarenko.evgenii@yandex.ru>
2023-09-03 11:52:17 +0300

    Задача Longest Substring Repeating Characters

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import twoSum
+import lengthOfLongestSubstring
 
 if __name__ == '__main__':
+
     nums = [2, 7, 11, 15]
     target = 9
-    print(twoSum.Solution().twoSum(nums, target)) # [0,1]
+    print("[0,1] = ",end='')
+    print(twoSum.Solution().twoSum(nums, target))
+
+    s = "abcabcbb"
+    print("3 = ",end='')
+    print(lengthOfLongestSubstring.Solution().lengthOfLongestSubstring(s))
 
 
